9f3ba594ba3e268e57b337fbe8cc25397fbd37e1
TaylorTrz <42276471+TaylorTrz@users.noreply.github.com>
2018-10-21 10:45:30 +0800

    连接Excel
    注意连接字符串的格式，以及数据驱动程序的下载；
    按照数据库格式，首行是作为Field的，不记录

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+namespace OpenExcel
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //OleDb
+            string conString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = E:\Desktop\6种调整模式.xlsx; Extended Properties= 'Excel 12.0; HDR=NO; IMEX=0' ";
+            string sql = "Select count(*) from [平面非均质$]";
+            OleDbConnection con = new OleDbConnection(conString);
+
+            try
+            {
+               con.Open();
+               Console.WriteLine("数据库连接成功");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            OleDbCommand com = new OleDbCommand(sql, con);
+            int i = Convert.ToInt32(com.ExecuteScalar());
+            Console.WriteLine("{0}", i);
+            Console.ReadKey();
+            con.Close();
+        }
+    }
+}
